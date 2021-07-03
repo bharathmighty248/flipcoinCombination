12a7b353c7924dc9231044f44ff264b2e750caa1
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-03 16:30:51 +0530

    doubletCombination

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,27 +1,38 @@
 #!/bin/bash -x
 
-Heads=0
-Tails=0
+HH=0
+HT=0
+TH=0
+TT=0
 
-declare -A singlet
+declare -A doublet
 read -p "Enter the simulation number : " n
 i=0
 while [[ $i != $n ]]
 do
-   flipcoin=$((RANDOM%2))
-   case $flipcoin in
-      1)
-         Heads=$(($Heads + 1))
-      ;;
-      0)
-         Tails=$(($Tails + 1))
-      ;;
-   esac
+   flipcoin1=$((RANDOM%2))
+   flipcoin2=$((RANDOM%2))
+   if [[ ($flipcoin1 == 1) && ($flipcoin2 == 1) ]]
+   then
+      HH=$(($HH + 1))
+   elif [[ ($flipcoin1 == 1) && ($flipcoin2 == 0) ]]
+   then
+      HT=$(($HT + 1))
+   elif [[ ($flipcoin1 == 0) && ($flipcoin2 == 1) ]]
+   then
+      TH=$(($TH + 1))
+   elif [[ ($flipcoin1 == 0) && ($flipcoin2 == 0) ]]
+   then
+      TT=$(($TT + 1))
+   fi
    ((i++))
 done
 
-singlet[H]=`echo "scale = 2; ($Heads/$n)*100" | bc`
-singlet[T]=`echo "scale = 2; ($Tails/$n)*100" | bc`
 
-echo singlet combinations ${!singlet[@]}
-echo singlet percentages  ${singlet[@]}
+doublet[HH]=`echo "scale = 2; ($HH/$n)*100" | bc`
+doublet[HT]=`echo "scale = 2; ($HT/$n)*100" | bc`
+doublet[TH]=`echo "scale = 2; ($TH/$n)*100" | bc`
+doublet[TT]=`echo "scale = 2; ($TT/$n)*100" | bc`
+
+echo doublet combinations ${!doublet[@]}
+echo combination percentages ${doublet[@]}
