7f6ffbebf5df285a31209f125391c083effbe778
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-03 16:26:04 +0530

    singlet combination

--- flipcoinCombination.sh
+++ flipcoinCombination.sh
@@ -1,11 +1,27 @@
 #!/bin/bash -x
 
-coinFlip=$((RANDOM%2))
+Heads=0
+Tails=0
 
-if [ $coinFlip -eq 1 ]
-then
-	echo Heads
-elif [ $coinFlip -eq 0 ]
-then
-	echo Tails
-fi
+declare -A singlet
+read -p "Enter the simulation number : " n
+i=0
+while [[ $i != $n ]]
+do
+   flipcoin=$((RANDOM%2))
+   case $flipcoin in
+      1)
+         Heads=$(($Heads + 1))
+      ;;
+      0)
+         Tails=$(($Tails + 1))
+      ;;
+   esac
+   ((i++))
+done
+
+singlet[H]=`echo "scale = 2; ($Heads/$n)*100" | bc`
+singlet[T]=`echo "scale = 2; ($Tails/$n)*100" | bc`
+
+echo singlet combinations ${!singlet[@]}
+echo singlet percentages  ${singlet[@]}
